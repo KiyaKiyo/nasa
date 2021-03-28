f59cd56144eafc0542ad3b69eba8c16716ba8621
KiyaKiyo <81028176+KiyaKiyo@users.noreply.github.com>
2021-03-28 19:03:32 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=p273rDXGhuF5vnsPKUxCJqwdkyd759PYObnTiqIy";
         
         $("#loadImage").click(function(){
             $.ajax({
